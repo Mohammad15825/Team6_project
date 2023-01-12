2d2c86f5bd27a6e902d767ce1fd142d2f1538189
mahbuba-jafar <120946574+mahbuba-jafar@users.noreply.github.com>
2023-01-12 21:16:19 +0400

    Update MenuServiceInter.java

--- service/inter/MenuServiceInter.java
+++ service/inter/MenuServiceInter.java
@@ -8,6 +8,7 @@ public interface MenuServiceInter {
 
     void getSectionOne(Path path) throws IOException;
     void getSectionTwo(Path path) throws IOException;
+    void getSectionThree(Path path) throws IOException;
     void getSectionFour(Path path) throws IOException;
     void getSectionFive(Path path) throws IOException;
 }
